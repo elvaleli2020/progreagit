79213006d67c2b8bcdb3d1b592d5338e925aa811
Elian Zapata <elvaleli@gmail.com>
2020-10-13 22:59:34 -0500

    Initial

---
+++ backend/src/main/java/co/edu/ufps/progreagit/model/AuthProvider.java
@@ -0,0 +1,8 @@
+package co.edu.ufps.progreagit.model;
+
+public enum  AuthProvider {
+    local,
+    google,
+    facebook,
+    github
+}
---
+++ backend/src/test/java/co/edu/ufps/progreagit/ProgreaGitApplicationTests.java
@@ -0,0 +1,14 @@
+package co.edu.ufps.progreagit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ProgreaGitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
