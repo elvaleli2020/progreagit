69919374f3bcd1d75556df0b8e248ee504abebe4
JhormanBD <majhor007@gmail.com>
2020-11-16 16:26:20 -0500

    Camos en buscarproyectos (Admin)

--- frontend/src/Components/Admin/Administrador.js
+++ frontend/src/Components/Admin/Administrador.js
@@ -5,15 +5,39 @@ import BuscaPersonas from "./BuscaPersonas";
 import BuscaProyectos from "./BuscaProyectos";
 import MediaAdmin from "./MediaAdmin";
 
+
 class Administrador extends Component {
     render() {
         return (
-            <div>
-                <AsignaRol/>
-                <BuscaPersonas/>
-                <BuscaProyectos/>
-                <MediaAdmin/>
+
+            <div className="container-fluid h-100">
+                <div className="row h-100">
+                    <aside className="col-12 col-md-2 p-0 bg-danger">
+                        <nav
+                            className="navbar navbar-expand navbar-dark bg-danger flex-md-column flex-row align-items-start">
+                            <div className="collapse navbar-collapse">
+                                <ul className="flex-md-column flex-row navbar-nav w-100 justify-content-between">
+                                    <li className="nav-item">
+                                        <a className="nav-link pl-0" href="#">Link</a>
+                                    </li>
+                                    ..
+                                </ul>
+                            </div>
+                        </nav>
+                    </aside>
+                    <main className="col">
+                        <BuscaProyectos></BuscaProyectos>
+                    </main>
+                </div>
+
+                {/*<AsignaRol/>*/}
+                {/*<BuscaPersonas/>*/}
+                {/*<MediaAdmin/>*/}
             </div>
+
+
+
+
 
         );
     }
--- frontend/src/Components/Admin/BuscaProyectos.js
+++ frontend/src/Components/Admin/BuscaProyectos.js
@@ -1,11 +1,67 @@
 import React, {Component} from 'react';
 import '../../Styles/Plantilla.css';
+import TableAdmin from "./TableAdmin";
 
 class BuscaProyectos extends Component {
     render() {
         return (
-            <h1>Este es el componente BuscaProyectos</h1>
-
+            <div>
+                <div >
+                    <h1>Búsqueda Avanzada</h1>
+                    <form className="col-sm-12">
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="InputTitle" placeholder="Título del proyecto"/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="InputAutores" placeholder="Autor(es)"/>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-9">
+                                <input type="text" className="form-control" id="InputKeywords"
+                                       placeholder="Palabras clave"/>
+                            </div>
+                            <div className="col-sm-3">
+                                <select id="inputEstado" className="form-control">
+                                    <option selected>Seleccione Estado:</option>
+                                    <option>Aceptada</option>
+                                    <option>Rechazada</option>
+                                    <option>Aceptada con correcciones</option>
+                                </select>
+                            </div>
+                        </div>
+                        <br/>
+                        <div className="row">
+                            <div className="col-sm">
+                                <input type="text" className="form-control" id="InputDateDesde"
+                                       placeholder="Fecha desde: (DD/MM/YYYY)"/>
+                            </div>
+                            <div className="col-sm">
+                                <input type="text" className="form-control" id="InputDateHasta"
+                                       placeholder="Fecha hasta: (DD/MM/YYYY)"/>
+                            </div>
+                            <div className="col-sm">
+                                <select id="inputCalificacion" className="form-control">
+                                    <option selected>Seleccione Calificación:</option>
+                                    <option>Aprobada</option>
+                                    <option>Reprobada</option>
+                                    <option>Laureada</option>
+                                    <option>Meritoria</option>
+                                </select>
+                            </div>
+                        </div>
+                        <br/>
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="InputDir"
+                                   placeholder="Director(es) / Codirector(es)"/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Buscar</button>
+                    </form>
+                </div>
+                <br/>
+               <div>
+                <TableAdmin></TableAdmin>
+               </div>
+            </div>
         );
     }
 }
