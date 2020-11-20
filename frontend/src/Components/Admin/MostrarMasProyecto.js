780a50f31300d2655d6ffc61a805c4e9f1e1014c
Elian Zapata <elvaleli@gmail.com>
2020-11-20 01:49:48 -0500

    datatable

---
+++ frontend/src/Components/Admin/MostrarMasProyecto.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class MostrarMasProyecto extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props);
+    }
+
+    render(){
+        return (
+            <div></div>
+        )
+    };
+
+}
+export default MostrarMasProyecto;
