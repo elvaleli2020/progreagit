41c0bb38c8b50389a91880b2138a5a8c5a9535bf
Elian Zapata <elvaleli@gmail.com>
2020-11-20 21:15:59 -0500

    Deployment

--- backend/src/main/java/co/edu/ufps/progreagit/ProgreaGitApplication.java
+++ backend/src/main/java/co/edu/ufps/progreagit/ProgreaGitApplication.java
@@ -4,10 +4,17 @@ import co.edu.ufps.progreagit.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-public class ProgreaGitApplication {
+//public class ProgreaGitApplication extends SpringBootServletInitializer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ProgreaGitApplication.class, args);
+//	}
+//}
+public class ProgreaGitApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgreaGitApplication.class, args);
