93fdc4ac4893629e0b7273c8dd1d8b8b348a8281
Elian Zapata <elvaleli@gmail.com>
2020-11-24 18:52:30 -0500

    readmi comienzos

---
+++ README.md
@@ -0,0 +1,66 @@
+![ProgreaGit](http://www.madarme.co/portada-web.png)
+# Título del proyecto:
+
+#### Ejemplo de Lectura de datos JSON 
+***
+## Índice
+1. [Características](#caracter-sticas-)
+2. [Contenido del proyecto](#contenido-del-proyecto)
+3. [Tecnologías](#tecnologías)
+4. [IDE](#ide)
+5. [Instalación](#instalación)
+6. [Demo](#demo)
+7. [Autor(es)](#autores)
+8. [Institución Académica](#institución-académica)
+***
+
+#### Características:
+
+  - Proyecto con lectura de datos json a través de la API fecth JavaScript
+  - Carga dinámica del JSON 
+
+***
+  #### Contenido del proyecto
+
+***
+#### Tecnologías
+
+  - HTML5
+  - TypeScript
+  - JavaScript
+  - Java
+  - Spring
+  - Hibernate
+	
+
+  ***
+#### IDE
+
+- El proyecto se desarrolla usando IntelJs
+- IDE de Spring -(https://www.jetbrains.com/idea/)
+
+***
+### Instalación
+
+***
+### Demo
+
+Para ver el demo de la aplicación puede dirigirse a: [ProgreaGit](http://progreagitv.cpsw.ingsistemasufps.co/).
+
+***
+### Autor(es)
+Proyecto desarrollado por 
+[Elian Nahun Zapata Alfonso COD:1151193 ] (<eliannahunza@ufps.edu.co>).
+[Jhorman Jesus Botello].
+[Juliana Andrea Ortega Castillo].
+
+
+***
+### Institución Académica   
+Proyecto desarrollado en la Materia programación web del  [Programa de Ingeniería de Sistemas] de la [Universidad Francisco de Paula Santander]
+
+
+
+   
+
+   
--- frontend/src/Components/Invitado/BuscaInvitado.js
+++ frontend/src/Components/Invitado/BuscaInvitado.js
@@ -5,25 +5,31 @@ class BuscaInvitado extends Component {
 
     render() {
         return (
+
             <div className=" col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                <div className="form-group row">
-                    <div className="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 form-inline">
-                        <label htmlFor="inputInvitado" className="col-12 col-sm-2 col-md-3 col-lg-3 col-xl-3">Buscar por: </label>
-                        <select id="inputInvitado" className=" form-control col-12 col-sm-10 col-md-9 col-lg-9 col-xl-9">
-                            <option selected>Estudiante</option>
-                            <option>Título</option>
-                            <option>Año</option>
-                        </select>
+                <div className="card">
+                    <div className="card-body">
+                        <div className="form-group row">
+                            <div className="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 form-inline">
+                                <label htmlFor="inputInvitado" className="col-12 col-sm-2 col-md-3 col-lg-3 col-xl-3">Buscar por: </label>
+                                <select id="inputInvitado" className=" form-control col-12 col-sm-10 col-md-9 col-lg-9 col-xl-9">
+                                    <option selected>Estudiante</option>
+                                    <option>Título</option>
+                                    <option>Año</option>
+                                </select>
+                            </div>
+                            <div className="col-12 col-sm-12 col-md-8 col-lg-9 col-xl-8">
+                                <input type="text" className="form-control col-12 col-sm-12 col-md-12" placeholder="Digite su búsqueda" id="buscarInvitado"/>
+                            </div>
+                            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-1">
+                                <button type="submit" className="btn btn-primary col-12 col-sm-6 col-md-6 col-lg-2 col-xl-12">Buscar</button>
+                            </div>
+                            <br/>
+                        </div>
                     </div>
-                    <div className="col-12 col-sm-12 col-md-8 col-lg-9 col-xl-8">
-                        <input type="text" className="form-control col-12 col-sm-12 col-md-12" placeholder="Digite su búsqueda" id="buscarInvitado"/>
-                    </div>
-                    <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-1">
-                        <button type="submit" className="btn btn-primary col-12 col-sm-6 col-md-6 col-lg-2 col-xl-12">Buscar</button>
-                    </div>
-                    <br/>
                 </div>
             </div>
+
         );
 
     }
