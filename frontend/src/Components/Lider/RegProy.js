2eca484bb5bb66e6ac61f4c349ad295882ba40b8
Juliana Ortega <julianaandreaoc@ufps.edu.co>
2020-11-28 15:20:37 -0500

    Parcialmente mejorada la vista de gestión de proyecto en el rol líder

--- frontend/src/Components/Lider/RegProy.js
+++ frontend/src/Components/Lider/RegProy.js
@@ -5,8 +5,7 @@ import BuscaPersonas from "../Admin/BuscaPersonas";
 import EliminaIntegrante from "./EliminaIntegrante";
 import Modal from 'react-bootstrap/Modal';
 import Button from "react-bootstrap/Button";
-import Integrantes from "./Integrante";
-
+import Integrantes,{cargarIntegrantes} from "./Integrante";
 
 
 class RegProy extends Component {
@@ -20,10 +19,12 @@ class RegProy extends Component {
             gitAddress: this.props.data.gitAddress,
             keywords:this.props.data.keywords,
             director: this.props.data.director,
-            date: this.props.data.startDate
+            date: this.props.data.startDate,
+            integrantes: this.props.cargarIntegrantes
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.eliminarIntegrante= this.eliminarIntegrante.bind(this);
+
     }
 
     eliminarIntegrante(event){
@@ -84,59 +85,65 @@ class RegProy extends Component {
             <div >
                 <h1>Gestión del proyecto</h1>
                 <form  onSubmit={this.handleSubmit} className="col-sm-12">
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="titulo" value={this.state.name} placeholder="Título del proyecto"
+                    <div className="form-group row col-sm-12">
+                        <label className="col-5 col-sm-3 col-md-2 col-lg-2 col-xl-1">Título del proyecto: </label>
+                        <input type="text" className="form-control col-7 col-sm-9 col-md-10 col-lg-10 col-xl-11 " id="titulo" value={this.state.name} placeholder="Título del proyecto"
                                onChange={(e)=>{this.setState({name: e.target.value})}} required/>
                     </div>
-                    <div className="row">
-                        <div className="col-sm-9">
-                            <input type="text" className="form-control" id="acronimo" value={this.state.acronym}
+                    <div className=" form-group row col-sm-12">
+                        <div className=" form-inline col-sm-7">
+                            <label className="col-5 col-sm-3 col-md-2 col-lg-2 col-xl-1">Acrónimo: </label>
+                            <input type="text" className="form-control col-7 col-sm-9 col-md-10 col-lg-10 col-xl-11" id="acronimo" value={this.state.acronym}
                                    placeholder="Sigla u Acrónimo" onChange={(e)=>{this.setState({acronym: e.target.value})}} required/>
                          </div>
-                        <div className="col-sm-3">
-                            <select id="inputClasificacion" defaultValue="proyecto_grado" className="form-control" disabled>
+                        <div className=" form-inline col-sm-5 " >
+                            <label className="col-5 col-sm-4 col-md-3 col-lg-3 col-xl-2">Clasificación: </label>
+                            <select id="inputClasificacion" defaultValue="proyecto_grado" className="form-control col-7 col-sm-8 col-md-9 col-lg-9 col-xl-10"
+                                    disabled>
                                 <option value="proyecto_grado">Proyecto de grado</option>
                             </select>
                         </div>
                     </div>
                     <br/>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="monitores" value={this.state.director}
-                               placeholder="Director(es) / Codirector(es)" onChange={(e)=>{this.setState({director: e.target.value})}} required/>
+                    <div className="form-group row col-sm-12">
+                        <label className="col-5 col-sm-3 col-md-2 col-lg-2 col-xl-1">Mentor(es)</label>
+                        <input type="text" className="form-control col-7 col-sm-9 col-md-10 col-lg-10 col-xl-11"
+                               id="monitores" value={this.state.director} placeholder="Director(es) / Codirector(es)"
+                               onChange={(e)=>{this.setState({director: e.target.value})}} required/>
                     </div>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="palabraClave" value={this.state.keywords}
-                               onChange={(e)=>{this.setState({keywords: e.target.value})}} placeholder="Palabras clave" required/>
+                    <div className="form-group row col-sm-12">
+                        <label className="col-5 col-sm-3 col-md-2 col-lg-2 col-xl-1">Palabras clave: </label>
+                        <input type="text" className="form-control col-7 col-sm-9 col-md-10 col-lg-10 col-xl-11"
+                               id="palabraClave" value={this.state.keywords} placeholder="Palabras clave"
+                               onChange={(e)=>{this.setState({keywords: e.target.value})}}  required/>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="txtAreaResum">Resumen del proyecto:</label>
-                        <textarea className="form-control" id="resumen" rows="5" value={this.state.abstracts}
+                    <div className="form-group row col-sm-12">
+                        <label className="col-sm-12">Resumen del proyecto:</label>
+                        <textarea className="form-control col-sm-12" id="resumen" rows="5" value={this.state.abstracts}
                                   onChange={(e)=>{this.setState({abstracts: e.target.value})}}/>
                     </div>
-                    <div className="row">
-                        <div className="col-sm-3">
-                            <input type="text" className="form-control" id="date" value={this.state.date}
-                                    disabled/>
+                    <div className="form-group row col-sm-12">
+                        <div className=" form-inline col-sm-3">
+                            <label className="form-group col-sm-6">Fecha de registro:</label>
+                            <input type="text" className="form-control col-sm-6" id="date" value={this.state.date}
+                                   disabled/>
                         </div>
-                        <div className="col-sm-1">
-                            <input type="text" className="form-control" id="InputDateHasta"
-                                   placeholder="Año" required/>
+                        <div className="form-inline col-sm-9">
+                            <label className=" col-sm-2">Integrantes: </label>
+                            <input type="text" className="form-control col-sm-7" id="InputGestionInt"
+                                   placeholder="Integrantes" />
+                            <Example className="col-sm-2" data={this.props.data.users} onAccion={this.eliminarIntegrante}></Example>
                         </div>
-                        <div className="col-sm-6">
-                            <input type="text" className="form-control" id="InputGestionInt"
-                                   placeholder="Integrantes:"/>
-                        </div>
-                        <div className="col-sm-2">
-                            <Example data={this.props.data.users} onAccion={this.eliminarIntegrante}></Example>
-                        </div>
+
                     </div>
                     <br/>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="repositorio" value={this.state.gitAddress}
+                    <div className="form-group row col-sm-12">
+                        <label className=" col-5 col-sm-3 col-md-2 col-lg-2 col-xl-1">Integrantes: </label>
+                        <input type="text" className="form-control col-7 col-sm-9 col-md-10 col-lg-10 col-xl-11 " id="repositorio" value={this.state.gitAddress}
                                placeholder="URL de repositorio" onChange={(e)=>{this.setState({gitAddress: e.target.value})}}/>
                     </div>
                     <br/>
-                    <button type="submit" className="btn btn-primary">Guardar</button>
+                    <button type="submit" className="btn btn-primary col-sm-2">Guardar</button>
                 </form>
             </div>
         );
